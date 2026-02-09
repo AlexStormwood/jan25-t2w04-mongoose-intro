bc65e2690059ffd270fc588e26c7fece6367f9df
Alex <alex.stormwood@bigfootds.com>
2026-02-09 20:46:07 +1100

    feat: database connection working
    Also have a workaround for a Node issue, to be fixed in Node 25.6.1

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 // Import the app
 const {app} = require("./server");
+const { dbConnect } = require("./utils/database");
 
 
 // Get the PORT environment variable 
@@ -7,13 +8,24 @@ const PORT = process.env.PORT || 3000;
 
 
 // Run the server 
-app.listen(PORT, () => {
-	// callback that runs when the server has successfully started
-	console.log(`Server is running on http://localhost:${PORT}`);
-});
+// app.listen(PORT, () => {
+// 	// callback that runs when the server has successfully started
+// 	console.log(`Server is running on http://localhost:${PORT}`);
+// });
 
 // Connect to the database 
-// TODO!!!
+dbConnect().then(() => {
+	app.listen(PORT, () => {
+		// callback that runs when the server has successfully started
+		console.log(`Server is running on http://localhost:${PORT}`);
+	});
+}).catch((error) => {
+	console.log(error);
+});
 
 
+
+// thingOne().then(() => {
+// 	thingTwo()
+// })
 
